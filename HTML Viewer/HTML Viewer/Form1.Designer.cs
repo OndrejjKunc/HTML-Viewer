699533313b1730eea8f7f527ec421df22d3e9d2a
OndrejjKunc <62773546+OndrejjKunc@users.noreply.github.com>
2021-02-03 23:08:55 +0100

    Add files via upload

---
+++ HTML Viewer/HTML Viewer/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HTML_Viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TreeViewer = new System.Windows.Forms.TreeView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.openFile = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TreeViewer
+            // 
+            this.TreeViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TreeViewer.Location = new System.Drawing.Point(3, 3);
+            this.TreeViewer.Name = "TreeViewer";
+            this.TreeViewer.Size = new System.Drawing.Size(780, 362);
+            this.TreeViewer.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 53);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(794, 394);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.TreeViewer);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(786, 368);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Prohlížeč stromu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Prohlížeč pravidel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.openFile, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // openFile
+            // 
+            this.openFile.Image = global::HTML_Viewer.Properties.Resources.folderOpen;
+            this.openFile.Location = new System.Drawing.Point(7, 7);
+            this.openFile.Margin = new System.Windows.Forms.Padding(7, 7, 3, 7);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(36, 36);
+            this.openFile.TabIndex = 2;
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TreeViewer;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button openFile;
+    }
+}
+
---
+++ HTML Viewer/HTML Viewer/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTML_Viewer
+{
+    public partial class Form1 : Form
+    {
+        Node root;
+        private void AddToTreeView(Node node, TreeNode parent = null)
+        {
+            if(node is Element)
+            {
+                Element e = (Element)node;
+                TreeNode newParent;
+                if (parent != null)
+                {
+                    parent.Nodes.Add($"Element: \"{e.TagName}\"");
+                    newParent = parent.Nodes[parent.Nodes.Count - 1];
+                }
+                else
+                {
+                    TreeViewer.Nodes.Add($"Element: \"{e.TagName}\"");
+                    newParent = TreeViewer.Nodes[0];
+                }
+                foreach (Attribute attribute in e.Attributes)
+                {
+                    newParent.Nodes.Add($"Attribute: \"{attribute.Name}\"");
+                    newParent.Nodes[0].Nodes.Add($"Value: \"{attribute.Value}\"");
+                }
+                foreach (Node child in e.Children)
+                {
+                    AddToTreeView(child, newParent);
+                }
+            }
+            else //node je Text
+            {
+                Text t = (Text)node;
+                parent.Nodes.Add($"Text: \"{t.Value}\"");
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "html files (*.html)|*.html";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string inputPath = openFileDialog.FileName;
+                string input = File.ReadAllText(inputPath);
+                Parser parser = new Parser(input);
+                root = parser.Parse();
+
+                AddToTreeView(root);
+                TreeViewer.ExpandAll();
+            }
+        }
+    }
+
+    public class Attribute
+    {
+        public string Name;
+        public string Value;
+
+        public Attribute(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    public class Element : Node
+    {
+        public string TagName;
+        public List<Attribute> Attributes = new List<Attribute>();
+        public List<Node> Children = new List<Node>();
+
+        public Element(string tagName, List<Attribute> attributes, List<Node> children)
+        {
+            TagName = tagName;
+            Attributes = attributes;
+            Children = children;
+        }
+    }
+
+    public class Text : Node
+    {
+        public string Value;
+        public Text(string t)
+        {
+            Value = t;
+        }
+    }
+
+    public class Node
+    {
+
+    }
+
+    public class Parser
+    {
+        int pos = 0; //Ukazatel soucasne pozice vstupu.
+        string input; //Text HTML vstupu.
+        List<string> selfClosingTags = new List<string>() { "area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr" };
+
+        private bool Eof()
+        {
+            return (pos >= input.Length);
+        }
+
+        private char ReadChar(bool advance) //Precte nasledujici znak inputu s moznosti posunout ukazatel pozice.
+        {
+            char c = input[pos];
+            if (advance)
+            {
+                pos++;
+            }
+            return c;
+        }
+
+        private string ReadWhile(Func<Boolean> condition) //Cte znaky dokud plati condition, vysledek vrati ve stringu.
+        {
+            string s = "";
+            while(condition())
+            {
+                s += ReadChar(true);
+            }
+            return s;
+        }
+
+        private void ConsumeWhitespace() //Posune ukazatel dokud nasledujici znak neni whitespace.
+        {
+            if (pos < input.Length)
+            {
+                ReadWhile(() => Char.IsWhiteSpace(ReadChar(false)));
+            }
+        }
+
+        private string ReadName()
+        {
+            return ReadWhile(() => ( Char.IsLetterOrDigit(ReadChar(false))) );
+        } //Precte alfanumericky nazev tagu nebo attributu.
+
+        private string ReadAttributeValue()
+        {
+            ReadChar(true); // "
+            string value = ReadWhile(() => ReadChar(false) != '"');
+            ReadChar(true); // "
+            return value;
+        } //Precte hodnotu ohranicenou uvozovkami.
+
+        private Attribute ParseAttribute()
+        {
+            string name = ReadName();
+            ReadChar(true); // =
+            string value = ReadAttributeValue();
+            return new Attribute(name, value);
+        }
+
+        private List<Attribute> ParseAttributes()
+        {
+            List<Attribute> attributes = new List<Attribute>();
+            while(true)
+            {
+                ConsumeWhitespace();
+                if (ReadChar(false) == '>')
+                {
+                    break;
+                }
+                Attribute attribute = ParseAttribute();
+                attributes.Add(attribute);
+            }
+            return attributes;
+        }
+        
+        private bool isSelfClosing(string name)
+        {
+            return selfClosingTags.Contains(name);
+        }
+
+        private Element ParseElement() //Precte a vrati element.
+        {
+            string tagName;
+            List<Attribute> attributes = new List<Attribute>();
+            List<Node> children = new List<Node>();
+
+            ReadChar(true); //<
+            tagName = ReadName();
+            attributes = ParseAttributes();
+            ReadChar(true); //>
+
+            if (!isSelfClosing(tagName))
+            {
+                children = ParseNodes();
+
+                ReadChar(true); // <
+                ReadChar(true); // /
+                ReadName();
+                ReadChar(true); // >
+            }
+
+            Element e = new Element(tagName, attributes, children);
+            return e;
+        }
+
+        private Text ParseText() //Precte a vrati text do zacatku nasledujiciho elementu.
+        {
+            Text t = new Text( ReadWhile(() => ReadChar(false) != '<') );
+            return t;
+        }
+
+        private List<Node> ParseNodes()
+        {
+            List<Node> nodes = new List<Node>();
+            while(true)
+            {
+                ConsumeWhitespace();
+                if (Eof() || input.Substring(pos, 2) == "</")
+                {
+                    break;
+                }
+                nodes.Add(ParseNode());
+            }
+            return nodes;
+        }
+
+        private Node ParseNode()
+        {
+            Node n = new Node();
+            if (ReadChar(false) == '<') //Element
+            {
+                n = ParseElement();
+            }
+            else //Text
+            {
+                n = ParseText();
+            }
+            return n;
+        }
+
+        public Node Parse() //Precte input a vrati root node, popr. prida root node pokud uz neexisture.
+        {
+            if (input.StartsWith("<!DOCTYPE html>"))
+            {
+                input = input.Remove(0, 15);
+                ConsumeWhitespace();
+            }
+
+            List<Node> nodes = ParseNodes();
+
+            Node node;
+            if (nodes.Count == 1)
+            {
+                node = nodes[0];
+            }
+            else
+            {
+                node = new Element("html", new List<Attribute>(), nodes);
+            }
+
+            return node;
+        }
+
+        public Parser(string _input)
+        {
+            input = _input;
+        }
+
+    }
+}
